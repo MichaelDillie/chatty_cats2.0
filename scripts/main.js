55fedbb2d0a1bdaf1c8946fe3ffb4fb879896914
Gabriel Bird <drmonumbo@gmail.com>
2015-09-25 18:18:46 -0500

    push test

--- scripts/main.js
+++ scripts/main.js
@@ -7,16 +7,23 @@ var chatCollection = require('./collections/listCollection.js');
 var chatView = require('./views/listView.js');
 var chatModel = require('./models/listModel.js');
 var chatView = require('./views/listView.js');
-var indChat = require('./views/indChat.js');
+// var indChat = require('./views/indChat.js');
 
 var chatCollection = new chatCollection();
-var chatModel = new chatModel();
-var chatView = new chatView();
-var indChat = new indChat({model: chatModel});
+var chatModel = new chatModel({collection: chatCollection});
+var chatView = new chatView({model: chatModel});
+// var indChat = new indChat({model: chatModel});
 
 $(document).ready(function() {
 console.log('ready');
 
+	var $makeYourProfile = $('#make-your-profile');
+	var $joinAChat = $('#join-a-chat');
+	var $learnMore = $('#learn-more');
+	var $startButton = $('#start-button');
+
+
+	// var profileView = new makeYourProfileView();
 
 setInterval(function() {
 $.get (
@@ -44,25 +51,11 @@ var addChat = function(e) {
 
 		)
 }
+var newChat = function(e) {
+	$('#newChatView').toggle();
+}
 
-// setInterval(function() {
-// 	$.ajax({
-// 		method: 'GET',
-// 		accepts: 'json',
-// 		url: 'https://chatty-cats.herokuapp.com/chats',
-// 		success: function(show) {
-// 			$('#main').html('');
-// 			console.log('hello');
-// 			for(var i=0; i<show.length; i++) {
-// 				$('#chatView').append('<div>' + show[i].user_id + ': ' + show[i].message + '</div>');
-// 			}
-// 		},
-// 		headers: {
-// 			'Cache-Control': 'no-cache'
-// 		}
-// 	});
-// }, 2000);
-
+$('#usersPop').on('click', newChat);
 $('#chatForm').on('submit', addChat);
 $('#submitChat').on('click', addChat);
 });
