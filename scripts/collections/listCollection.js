a3f7c141e15371239ab91e235517f5c498410843
Gabriel Bird <drmonumbo@gmail.com>
2015-09-25 12:46:40 -0500

    Revert "Initialized chat page elements"
    This reverts commit e6e8bc787eaf2b718b8cd2f02c4e18ed33054581.

--- scripts/collections/listCollection.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-var listModel = require('../models/listModel.js');
-var Backbone = require('backbone');
-module.exports = Backbone.Collection.extend({
-	model: listModel
-
-});
-console.log('i am a collection');
